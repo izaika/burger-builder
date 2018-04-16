9977577e2d203daae9d073b5bb1c24f5f756a3c9
Igor Zaika <zaika.igor@pdffiller.team>
2018-04-16 05:50:40 +0300

    113. Starting Implementation of The Burger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React, { Fragment } from 'react';
+import classes from './Layout.css';
 
 const Layout = props => (
   <Fragment>
     <div>Toolbar, SideDrawer</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Fragment>
 );
 
