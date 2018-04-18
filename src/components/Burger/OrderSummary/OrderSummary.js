cf0ab0ed913998575cb6ede0a5841028f74a6055
Igor Zaika <zaika.igor@pdffiller.team>
2018-04-18 23:04:06 +0300

    130. Adding the Price to the Order Summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -14,6 +14,9 @@ const orderSummary = props => {
       <h3>Your order</h3>
       <p>A delicious burger with the following ingredients:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: ${props.price.toFixed(2)}</strong>
+      </p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCanceled}>
         CANCEL
