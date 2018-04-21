45565224539e46b355fb186089c49543c31bd281
Igor Zaika <zaika.igor@pdffiller.team>
2018-04-21 09:36:49 +0300

    142. Improving Performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,34 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(igKey => (
-    <li key={igKey}>
-      <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
-    </li>
-  ));
+class OrderSummary extends Component {
+  render() {
+    const { props } = this;
+    const ingredientSummary = Object.keys(props.ingredients).map(igKey => (
+      <li key={igKey}>
+        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
+      </li>
+    ));
 
-  return (
-    <Fragment>
-      <h3>Your order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: ${props.price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanceled}>
-        CANCEL
-      </Button>
-      <Button btnType="Success" clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
+    return (
+      <Fragment>
+        <h3>Your order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: ${props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={props.purchaseCanceled}>
+          CANCEL
+        </Button>
+        <Button btnType="Success" clicked={props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
