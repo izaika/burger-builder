8c627d8a9e16dc4526cb9c4dcd8f1a6344c93d87
Igor Zaika <zaika.igor@pdffiller.team>
2018-05-18 16:40:28 +0300

    311. Getting a Token from the Backend

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,7 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
+import secret from '../../secret';
 
 export const authStart = () => ({ type: actionTypes.AUTH_START });
 export const authSuccess = authData => ({ type: actionTypes.AUTH_SUCCESS, authData });
@@ -6,4 +9,17 @@ export const authFail = error => ({ type: actionTypes.AUTH_FAIL, error });
 
 export const auth = (email, password) => async dispatch => {
   dispatch(authStart());
+  try {
+    const response = await axios({
+      method: 'post',
+      url: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser',
+      params: { key: secret.authFirebaseKey },
+      data: { email, password, returnSecureToken: true }
+    });
+    console.log(response.data);
+    dispatch(authSuccess(response.data));
+  } catch (e) {
+    console.error(e);
+    dispatch(authFail(e));
+  }
 };
