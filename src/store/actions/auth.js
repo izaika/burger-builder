4518189086fb3da76528a25e5bcb790d0204d094
Igor Zaika <zaika.igor@pdffiller.team>
2018-05-18 17:54:24 +0300

    315. Logging Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,20 +7,27 @@ export const authStart = () => ({ type: actionTypes.AUTH_START });
 export const authSuccess = (idToken, userId) => ({ type: actionTypes.AUTH_SUCCESS, idToken, userId });
 export const authFail = error => ({ type: actionTypes.AUTH_FAIL, error });
 
+export const logOut = () => ({ type: actionTypes.AUTH_LOGOUT });
+
+export const checkAuthTimeout = expirationTime => async dispatch => {
+  setTimeout(() => {
+    dispatch(logOut());
+  }, expirationTime * 1000);
+};
+
 export const auth = (email, password, isSignup) => async dispatch => {
   dispatch(authStart());
   try {
-    let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser';
-    if (!isSignup) url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword';
-
+    const baseUrl = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty';
     const response = await axios({
       method: 'post',
-      url,
+      url: isSignup ? `${baseUrl}/signupNewUser` : `${baseUrl}/verifyPassword`,
       params: { key: secret.authFirebaseKey },
       data: { email, password, returnSecureToken: true }
     });
     console.log(response.data);
     dispatch(authSuccess(response.data.idToken, response.data.localId));
+    dispatch(checkAuthTimeout(response.data.expiresIn));
   } catch (error) {
     dispatch(authFail(error.response.data.error));
   }
