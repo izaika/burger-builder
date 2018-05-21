ed771d45978b06d05f5fed72de32a9a98c794ab5
Igor Zaika <zaika.igor@pdffiller.team>
2018-05-21 18:11:07 +0300

    324. Guarding Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Switch, Route, withRouter } from 'react-router-dom';
+import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 
 import * as actions from './store/actions';
 import Layout from './containers/Layout/Layout';
@@ -16,18 +16,35 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout} />
           <Route path="/orders" component={Orders} />
-          <Route path="/auth" component={Auth} />
           <Route path="/logout" component={Logout} />
           <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
         </Switch>
-      </Layout>
-    );
+      );
+    }
+
+    return <Layout>{routes}</Layout>;
   }
 }
 
-export default withRouter(connect(null, { onTryAutoSignUp: actions.authCheckState })(App));
+export default withRouter(
+  connect(
+    state => ({
+      isAuthenticated: !!state.auth.token
+    }),
+    { onTryAutoSignUp: actions.authCheckState }
+  )(App)
+);
